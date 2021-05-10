330ac092782ba8f6c33a14f248c83f0287c9144d
DataWorks-CI <48289519+DataWorks-CI@users.noreply.github.com>
2021-05-10 13:55:09 +0000

    Initial commit, adding githooks submodule

--- ecr.tf
+++ ecr.tf
@@ -1,16 +1,16 @@
-resource "aws_ecr_repository" "dataworks-repo-template-docker" {
-  name = "dataworks-repo-template-docker"
+resource "aws_ecr_repository" "dataworks-alertmanager-sns-forwarder" {
+  name = "dataworks-alertmanager-sns-forwarder"
   tags = merge(
     local.common_tags,
-    { DockerHub : "dwpdigital/dataworks-repo-template-docker" }
+    { DockerHub : "dwpdigital/dataworks-alertmanager-sns-forwarder" }
   )
 }
 
-resource "aws_ecr_repository_policy" "dataworks-repo-template-docker" {
-  repository = aws_ecr_repository.dataworks-repo-template-docker.name
+resource "aws_ecr_repository_policy" "dataworks-alertmanager-sns-forwarder" {
+  repository = aws_ecr_repository.dataworks-alertmanager-sns-forwarder.name
   policy     = data.terraform_remote_state.management.outputs.ecr_iam_policy_document
 }
 
 output "ecr_example_url" {
-  value = aws_ecr_repository.dataworks-repo-template-docker.repository_url
+  value = aws_ecr_repository.dataworks-alertmanager-sns-forwarder.repository_url
 }
